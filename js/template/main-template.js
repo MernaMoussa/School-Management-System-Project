409292a79e8c9364985913425568086e25a40b0a
Merna Moussa <mernahelmy.m@gmail.com>
2023-11-15 16:07:27 +0100

    Adjust 'layout.css' file and 'index.html' to use the grid foe the main content only

--- js/template/main-template.js
+++ js/template/main-template.js
@@ -1,12 +1,8 @@
 export default function mainTemplate(data) {
-    return `
+  return `
       <div class="wrapper">
-        <div class="header-logo">${data.header}</div>
-        <div class="header-nav">${data.header}</div>
-        <div class="header-login">${data.header}</div>
         <div class="content-left">${data.contentLeft}</div>
         <div class="content-right">${data.contentRight}</div>
-        <div class="footer">${data.footer}</div>
       </div>
     `;
 };
--- script.js
+++ script.js
@@ -5,10 +5,8 @@ const acs = {};
 acs.init = () => {
     const mainContainer = document.querySelector('.main-container');
     const templateData = {
-        header: 'Header Content',
         contentLeft: 'Left Content',
         contentRight: 'Right Content',
-        footer: 'Footer Content',
     };
     mainContainer.innerHTML = mainTemplate.render(templateData);
 }
