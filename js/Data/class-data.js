e89fbc18bd1ddd37e2d19d63730a3f2ca021a22f
Merna Moussa <mernahelmy.m@gmail.com>
2023-11-15 12:51:12 +0100

    Add data in 'class-data.js', 'students-data.js'and 'teachers-data.js'

---
+++ js/Data/class-data.js
@@ -0,0 +1,27 @@
+const classes = [
+    {
+        id: 1,
+        name: "Fullstack Class",
+        teacherIds: [101, 102, 103],
+    },
+    {
+        id: 2,
+        name: "Cloud Class",
+        teacherIds: [104, 105, 106],
+    },
+    {
+        id: 3,
+        name: "Data Science Class",
+        teacherIds: [107, 108, 109],
+    },
+    {
+        id: 4,
+        name: "Mobile Class",
+        teacherIds: [110, 111, 112],
+    },
+    {
+        id: 5,
+        name: "Security Class",
+        teacherIds: [113, 114, 115],
+    },
+];
---
+++ js/Data/teachers-data.js
@@ -0,0 +1,92 @@
+const teachers = [
+    {
+        id: 101,
+        name: "Mr. Anderson",
+        department: "fullstack",
+        subject: "programming",
+    },
+    {
+        id: 102,
+        name: "Mrs. Roberts",
+        department: "fullstack",
+        subject: "webDevelopment",
+    },
+    {
+        id: 103,
+        name: "Dr. Smith",
+        department: "fullstack",
+        subject: "databases",
+    },
+    {
+        id: 104,
+        name: "Prof. Davis",
+        department: "cloud",
+        subject: "cloudComputing",
+    },
+    {
+        id: 105,
+        name: "Ms. Turner",
+        department: "cloud",
+        subject: "virtualization",
+    },
+    {
+        id: 106,
+        name: "Mr. Johnson",
+        department: "cloud",
+        subject: "networking",
+    },
+    {
+        id: 107,
+        name: "Dr. White",
+        department: "dataScience",
+        subject: "machineLearning",
+    },
+    {
+        id: 108,
+        name: "Prof. Lee",
+        department: "dataScience",
+        subject: "dataAnalytics",
+    },
+    {
+        id: 109,
+        name: "Ms. Clark",
+        department: "dataScience",
+        subject: "statistics",
+    },
+    {
+        id: 110,
+        name: "Mrs. Brown",
+        department: "mobile",
+        subject: "mobileDevelopment",
+    },
+    {
+        id: 111,
+        name: "Dr. Taylor",
+        department: "mobile",
+        subject: "appDesign",
+    },
+    {
+        id: 112,
+        name: "Prof. Cooper",
+        department: "mobile",
+        subject: "appTesting",
+    },
+    {
+        id: 113,
+        name: "Mr. Adams",
+        department: "security",
+        subject: "networkSecurity",
+    },
+    {
+        id: 114,
+        name: "Mrs. Rogers",
+        department: "security",
+        subject: "cybersecurity",
+    },
+    {
+        id: 115,
+        name: "Dr. Harris",
+        department: "security",
+        subject: "encryption",
+    },
+];
