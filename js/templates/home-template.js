1fe465c8818fc9fbb1a01b75714d2b416a2f2bec
Merna Moussa <mernahelmy.m@gmail.com>
2023-11-18 14:19:12 +0100

    Export the countedelements from 'home-module.js' and use them in 'home-template.js'

--- js/templates/home-template.js
+++ js/templates/home-template.js
@@ -1,14 +1,16 @@
-export default function template(pNumberOfStudents) {
+export default function template(pCountedStudents, pCountedTeachers, pCountedClassses) {
   return `
   <div class="wrapper" id="page1">
     <div class="left-content">
-      <p>${pNumberOfStudents}</p>
+      <p>${pCountedStudents}</p>
       <p>number of students</p>
     </div>
     <div class="middle-content">
+    <p>${pCountedTeachers}</p>
       <p>middle content</p>
     </div>
     <div class="right-content">
+    <p>${pCountedClassses}</p>
       <p>right content</p>
     </div>
   </div>
