4ffbc9c0fb9c6326bf4d00e6e15cf24c2dc5989f
Merna Moussa <mernahelmy.m@gmail.com>
2023-11-26 22:04:11 +0100

    Adjust the 'classes.js' to render its data from 'classes-data.js' and put the rendered data in the 'content.template.js'

--- pages/classes.js
+++ pages/classes.js
@@ -1,11 +1,11 @@
 import classes from "../school-data/classes-data.js";
-function template() {
-    return classes.map(cls => `
+function template(pClasses) {
+    return pClasses.map(pClass => `
         <div class="wrapper">
             <button type="button" id="edit"><i class="fa fa-regular fa-pen-to-square"></i></button>
             <button type="button" id="remove"><i class="fa fa-regular fa-circle-minus"></i></button>  
-            <h2>${cls.name}</h2>
-            <p>Teacher IDs: ${cls.teacherIds.join(', ')}</p>
+            <h2>${pClass.name}</h2>
+            <p>Teacher IDs: ${pClass.teacherIds.join(', ')}</p>
             <p>Additional text</p>
             <button type="button" id="students">Students</button>
             <button type="button" id="teachers">Teachers</button>
