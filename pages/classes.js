72e8454d4b7b001caa62d5c712dd4a3a43a38ea1
Merna Moussa <mernahelmy.m@gmail.com>
2023-11-26 22:26:57 +0100

    Add some classes for the elements to give them style

--- pages/classes.js
+++ pages/classes.js
@@ -1,17 +1,18 @@
 import classes from "../school-data/classes-data.js";
+
 function template(pClasses) {
     return pClasses.map(pClass => `
-        <div class="wrapper">
-            <button type="button" id="edit"><i class="bi bi-pencil"></i></button>
-            <button type="button" id="remove"><i class="bi bi-dash-circle"></i></button>  
-            <h2>${pClass.name}</h2>
-            <p>Teacher IDs: ${pClass.teacherIds.join(', ')}</p>
-            <p>Additional text</p>
-            <button type="button" id="students">Students</button>
-            <button type="button" id="teachers">Teachers</button>
-        </div>
-    `).join('');
+    <div class="wrapper d-inline-block text-center m-2 p-3 border">
+      <button type="button" id="edit" class="btn btn-primary"><i class="bi bi-pencil"></i></button>
+      <button type="button" id="remove" class="btn btn-danger"><i class="bi bi-dash-circle"></i></button>
+      <h2>${pClass.name}</h2>
+      <p>Teacher IDs: ${pClass.teacherIds.join(', ')}</p>
+      <p>Additional text</p>
+      <button type="button" id="students" class="btn btn-info">Students</button>
+      <button type="button" id="teachers" class="btn btn-info">Teachers</button>
+    </div>
+  `).join('');
 }
 
 const classesTemplate = template(classes);
-export default classesTemplate
+export default classesTemplate;
