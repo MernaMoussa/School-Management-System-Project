509ec23a4293764b7fc9eda79f3ad78f64b5a520
Merna Moussa <mernahelmy.m@gmail.com>
2023-11-26 22:32:31 +0100

    align buttons in the right and give them margin

--- pages/classes.js
+++ pages/classes.js
@@ -3,8 +3,10 @@ import classes from "../school-data/classes-data.js";
 function template(pClasses) {
     return pClasses.map(pClass => `
     <div class="wrapper d-inline-block text-center m-2 p-3 border">
-      <button type="button" id="edit" class="btn btn-primary"><i class="bi bi-pencil"></i></button>
-      <button type="button" id="remove" class="btn btn-danger"><i class="bi bi-dash-circle"></i></button>
+    <div class="d-flex justify-content-end">
+      <button type="button" id="edit" class="btn btn-primary m-1"><i class="bi bi-pencil"></i></button>
+      <button type="button" id="remove" class="btn btn-danger m-1"><i class="bi bi-dash-circle"></i></button>
+    </div>
       <h2>${pClass.name}</h2>
       <p>Teacher IDs: ${pClass.teacherIds.join(', ')}</p>
       <p>Additional text</p>
