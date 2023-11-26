c14edddca03d9655ee7338bc66afc21e4eac50de
Merna Moussa <mernahelmy.m@gmail.com>
2023-11-26 22:58:32 +0100

    Add 'show active' class to the home again

--- components/content/content.template.js
+++ components/content/content.template.js
@@ -3,8 +3,8 @@ import classesTemplate from "../../pages/classes.js"
 export default function contentTemplate() {
     return `
 <div class="tab-content" id="pills-tabContent">
-    <div class="tab-pane fade " id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">${homeTemplate}</div>
-    <div class="tab-pane fade show active d-flex flex-wrap justify-content-center" id="pills-classes" role="tabpanel" aria-labelledby="pills-classes-tab" tabindex="0">${classesTemplate}</div>
+    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">${homeTemplate}</div>
+    <div class="tab-pane fade d-flex flex-wrap justify-content-center" id="pills-classes" role="tabpanel" aria-labelledby="pills-classes-tab" tabindex="0">${classesTemplate}</div>
     <div class="tab-pane fade" id="pills-teachers" role="tabpanel" aria-labelledby="pills-teachers-tab" tabindex="0">...</div>
     <div class="tab-pane fade" id="pills-students" role="tabpanel" aria-labelledby="pills-students-tab" tabindex="0">...</div>
 </div>  `
